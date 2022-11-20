84f56c7e5267870f024326434e31b713d505b4e5
IdanLnrd <idangibly.dev@gmail.com>
2022-11-20 20:04:06 +0200

    Update Start.tsx

--- client/src/components/Start.tsx
+++ client/src/components/Start.tsx
@@ -1,12 +1,13 @@
-import { FunctionComponent, useCallback, useEffect, useState } from "react";
+import { FunctionComponent, useCallback, useEffect, useRef, useState } from "react";
 import { Utils } from "../utils";
 import Shape, { GameShapes, getRandomShape } from "./Shape";
 enum Sides {
   LEFT, RIGHT
 }
-const sides: Sides[] = Object.values(Sides) as Sides[];
+const sides = [ Sides.LEFT, Sides.RIGHT ];
 const getRandomeSide = () => {
-  return [Sides.LEFT, Sides.RIGHT][Math.floor(Math.random() * sides.length)];
+  const index = Math.floor(Math.random() * sides.length);
+  return sides[index];
 }
 
 async function updateUserScore(name: string) {
@@ -15,11 +16,15 @@ async function updateUserScore(name: string) {
 }
 
 const Start: FunctionComponent<{ name: string }> = ({ name }) => {
+  const ref = useRef<HTMLDivElement>(null);
   const [loading, setLoading] = useState(true);
   const [message, setMessage] = useState('');
-  const [progress, setProgress] = useState(0);
-  const [indicator, setIndicator] = useState<0 | 1 | 2>(0);
-  const [state, setState] = useState<{ shape: GameShapes, side: Sides }>({ side: getRandomeSide(), shape: getRandomShape() });
+  const [score, setScore] = useState(0);
+  const [userSide, setUserSide] = useState<Sides | undefined>();
+  const [indicator, setIndicator] = useState<0 | 1 | 2>(1);
+  const initShape = getRandomShape();
+  const initSide = getRandomeSide();
+  const [state, setState] = useState<{ shape: GameShapes, side: Sides }>({ shape: initShape, side: initSide });
 
   const updateIndicator = useCallback(async () => {
     setIndicator(1);
@@ -28,20 +33,30 @@ const Start: FunctionComponent<{ name: string }> = ({ name }) => {
   }, []);
 
   useEffect(() => {
-    const updateLoader = async () => {
+    const init = async () => {
       await Utils.wait(2000 + Math.floor(Math.random() * 3000));
       setLoading(false);
     }
-    updateLoader();
+    init();
   }, []);
 
+  useEffect(() => {
+    updateIndicator();
+  }, [state, updateIndicator]);
+
   const next = () => {
-    setState({ side: getRandomeSide(), shape: getRandomShape() });
     setMessage('');
+    setState(() => ({ side: getRandomeSide(), shape: getRandomShape() }));
   }
 
   const handleUserInput = (side: Sides) => {
-
+    setUserSide(side);
+    if (state.side === side) {
+      setScore(score + 1);
+      setMessage('Greate!');
+    } else {
+      setMessage('Ho no...');
+    }
   }
 
   const handleKeyDown = (key: string) => {
@@ -52,7 +67,13 @@ const Start: FunctionComponent<{ name: string }> = ({ name }) => {
       return setMessage('Too Soon');
     }
     if(indicator === 2) {
+      if (userSide !== undefined) {
+        return next();
+      }
       return setMessage('Too Late');
+    }
+    if (userSide !== undefined) {
+      return;
     }
     if (key === 'a') { handleUserInput(Sides.LEFT) }
     if (key === 'l') { handleUserInput(Sides.RIGHT) }
@@ -60,25 +81,27 @@ const Start: FunctionComponent<{ name: string }> = ({ name }) => {
   if (loading) {
     return <div>Loading...</div>
   }
-  const { shape, side } = state;
-  return <div 
-    tabIndex={0} style={{ height: '100%' }}  
+  setTimeout(() => {
+    ref.current?.focus();
+  }, 0)
+  return <div ref={ref} tabIndex={0} style={{ height: '100%' }}  
     onKeyDown={({ key }) => handleKeyDown(key)}>
-    <div>User name: {name}</div>
+    <div style={{ margin: '3rem' }}>User name: {name}</div>
     <div>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div style={{ width: '200px' }}>
-        <Shape hide={side === Sides.LEFT} shape={shape}/>
+        <Shape hide={state.side === Sides.RIGHT} shape={state.shape}/>
       </div>
       <div style={{ background: 'black', width: '1px', height: '100px', margin: '16px' }}></div>
       <div style={{ width: '200px' }}>
-        <Shape hide={side === Sides.RIGHT} shape={shape}/>
+        <Shape hide={state.side === Sides.LEFT} shape={state.shape}/>
       </div>
       </div>
       
     </div>
-    <div>{ indicator === 1  ? 'You can play' : '' }</div>
+    <h1>{ indicator === 1  ? 'You can play' : '' }</h1>
     <div>{message}</div>
+    <div>Score: {score}</div>
   </div>
 }
 
