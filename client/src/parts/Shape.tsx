867901175c8eb5df0a713bec07d77b258ad09de2
IdanLnrd <idangibly.dev@gmail.com>
2022-11-23 23:50:12 +0200

    adding the shapes files

--- client/src/parts/Shape.tsx
+++ client/src/parts/Shape.tsx
@@ -1,11 +1,16 @@
-import { Image, Text } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 import { FunctionComponent, memo } from "react";
 import { Shapes } from "../utils/constants";
 export interface ShapeProps {
   shape: Shapes;
   hide?: boolean;
+  size?: number;
 }
-const Shape: FunctionComponent<ShapeProps> = memo(({ shape, hide}) => {
-  return <Text hidden={hide}>{shape}</Text>
+const Shape: FunctionComponent<ShapeProps> = memo(({ shape, hide, size = 200 }) => {
+  return <Image
+   src={`/shapes/${shape}.svg`} 
+   width={size}
+   height={size}
+   hidden={hide} />
 });
 export default Shape;
